a34b9d29aaf40359c31099e1ee7ae60f8706477e
hun <hunminy0986@gmail.com>
2026-02-24 14:20:55 +0900

    port number fix

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
